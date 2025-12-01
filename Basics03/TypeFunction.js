01a2bff459ee646fb1797e97306197bea26f0e13
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2025-12-01 13:16:10 +0000

    TypeOfFunction

--- Basics03/TypeFunction.js
+++ Basics03/TypeFunction.js
@@ -1,9 +1,11 @@
+// normal function
 function addone (num){
     return num + " => normal"
 }
 console.log(addone("First type of function"))
 
+// store function into variable
 const addtwo = function(num){
-    return num + " => add in variable"
+    return num + " => store in variable"
 }
 console.log(addtwo("Second type of function"))
